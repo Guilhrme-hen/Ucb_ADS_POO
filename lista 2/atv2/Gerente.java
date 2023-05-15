6385c142e8b1e7cc3b32208492e1b0dcdea3ec59
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2023-05-15 19:58:20 -0300

    Add files via upload

---
+++ lista 2/atv2/Gerente.java
@@ -0,0 +1,14 @@
+package empresa.teste;
+
+public class Gerente extends Empregado {
+    
+    private String nomeGerencia;
+
+    public String getNomeGerencia() {
+        return nomeGerencia;
+    }
+
+    public void setNomeGerencia(String nomeGerencia) {
+        this.nomeGerencia = nomeGerencia;
+    }
+}
