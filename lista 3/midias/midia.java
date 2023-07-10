134e98b29a6d13571d09105690aa7da6455d8ce0
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2023-07-09 22:56:17 -0300

    Update midia.java

--- lista 3/midias/midia.java
+++ lista 3/midias/midia.java
@@ -1,1 +1,59 @@
+package cadastromidias;
 
+public class Midia {
+     private int codigo;
+    private double preco;
+    private String nome;
+
+    public Midia() {
+    }
+
+    public Midia(int codigo, double preco, String nome) {
+        this.codigo = codigo;
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return "Midia";
+    }
+
+    public String getDetalhes() {
+        return "Codigo: " + codigo + ", Nome: " + nome + " Preço: " + preco + ",;
+    }
+
+    public void printDados() {
+        System.out.println("Tipo: " + getTipo());
+        System.out.println("Detalhes: " + getDetalhes());
+    }
+
+    public void inserirDados(int codigo, double preco, String nome) {
+        this.codigo = codigo;
+        this.preco = preco;
+        this.nome = nome;
+    }
+}
