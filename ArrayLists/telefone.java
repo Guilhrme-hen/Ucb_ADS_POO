c2cc903c2fb9fe474689a6b64fb9f2c670e23d8d
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2023-07-09 22:38:35 -0300

    Create telefone.java

---
+++ ArrayLists/telefone.java
@@ -0,0 +1,37 @@
+public class Telefone {
+    private String tipo;
+    private String ddd;
+    private String numero;
+
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    } 
+    
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+    
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+    
+    public void imprimirTelefone() {
+        System.out.println("Tipo: " + tipo);
+        System.out.println("(DDD): " + ddd);
+        System.out.println("Numero: " + numero);
+    }
+
+}
